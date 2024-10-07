02045812a7ae5f01723b39e144acb6457f63a7b5
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2024-10-07 12:31:38 +0100

    Commit

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    host:'192.168.1.244',
-    port:5176
-  },
+      
   plugins: [react()],
 })
